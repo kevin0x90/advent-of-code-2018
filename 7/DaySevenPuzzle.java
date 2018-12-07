3e414d7f9481049abd4b104c8513e563d469d6b8
k3v1n <k3v1n0x90@gmail.com>
2018-12-07 22:09:57 +0100

    Update day seven part a solution since wrong version of the code was commited

--- 7/DaySevenPuzzle.java
+++ 7/DaySevenPuzzle.java
@@ -5,69 +5,62 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.TreeSet;
 import java.util.Set;
-import java.util.ArrayList;
 
 class Solver {
-  public static void main(final String[] args) throws FileNotFoundException {
-    final File input = new File("C:\\Users\\k3v1n\\Documents\\adventOfCode\\7\\DaySevenPuzzle.txt");
-    final Map<String, Set<String>> adjacencyList = new HashMap<>();
-    final Map<String, Set<String>> parentList = new HashMap<>();
-    String rootNode = "";
+    public static void main(final String[] args) throws FileNotFoundException {
+        final File input = new File("C:\\Users\\k3v1n\\Documents\\adventOfCode\\7\\DaySevenPuzzle.txt");
+        final Map<String, Set<String>> adjacencyList = new HashMap<>();
+        final Map<String, Set<String>> parentList = new HashMap<>();
 
-    try (final Scanner scanner = new Scanner(input)) {
-      while (scanner.hasNext()) {
-        scanner.next();
-        final String before = scanner.next();
+        try (final Scanner scanner = new Scanner(input)) {
+            while (scanner.hasNext()) {
+                scanner.next();
+                final String before = scanner.next();
 
-        if (rootNode == "") {
-          rootNode = before;
+                scanner.next();
+                scanner.next();
+                scanner.next();
+                scanner.next();
+                scanner.next();
+                final String after = scanner.next();
+                scanner.nextLine();
+
+                if (parentList.containsKey(after)) {
+                    parentList.get(after).add(before);
+                } else {
+                    final Set<String> parentNodes = new TreeSet<>();
+                    parentNodes.add(before);
+                    parentList.put(after, parentNodes);
+                }
+
+                if (adjacencyList.containsKey(before)) {
+                    adjacencyList.get(before).add(after);
+                } else {
+                    final Set<String> nodes = new TreeSet<>();
+                    nodes.add(after);
+                    adjacencyList.put(before, nodes);
+                }
+
+                adjacencyList.putIfAbsent(after, new TreeSet<>());
+            }
         }
 
-        scanner.next();
-        scanner.next();
-        scanner.next();
-        scanner.next();
-        scanner.next();
-        final String after = scanner.next();
-        scanner.nextLine();
+        while (adjacencyList.size() > 0) {
 
-        if (parentList.containsKey(after)) {
-          parentList.get(after).add(before);
-        } else {
-          final Set<String> parentNodes = new TreeSet<>();
-          parentNodes.add(before);
-          parentList.put(after, parentNodes);
+            String rootNode = "";
+            for (String parent : adjacencyList.keySet()) {
+                if (!parentList.containsKey(parent) || parentList.get(parent).size() == 0) {
+                    rootNode = parent;
+                    break;
+                }
+            }
+
+            for (final String child : adjacencyList.get(rootNode)) {
+                parentList.get(child).remove(rootNode);
+            }
+
+            adjacencyList.remove(rootNode);
+            System.out.print(rootNode);
         }
-
-        if (adjacencyList.containsKey(before)) {
-          adjacencyList.get(before).add(after);
-        } else {
-          final Set<String> nodes = new TreeSet<>();
-          nodes.add(after);
-          adjacencyList.put(before, nodes);
-        }
-      }
     }
-
-    System.out.println(parentList);
-    System.out.println(adjacencyList);
-    
-    walkTree(rootNode, adjacencyList, parentList);
-    
-  }
-
-  private static void walkTree(String rootNode, Map<String, Set<String>> adjacencyList, Map<String, Set<String>> parentList) {
-    System.out.print(rootNode);
-
-    if (adjacencyList.get(rootNode) == null) {
-      return;
-    }
-
-    for (final String child : adjacencyList.get(rootNode)) {
-      parentList.get(child).remove(rootNode);
-      if (parentList.get(child).size() == 0) {
-        walkTree(child, adjacencyList, parentList);
-      }
-    }
-  }
 }
